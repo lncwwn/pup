b7da1e025cda5c74c1b7b792966c0d4047582217
victor <lncwwn@gmail.com>
2015-05-03 23:16:18 +0800

    added new route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('index');
+});
+
+
 module.exports = router;
